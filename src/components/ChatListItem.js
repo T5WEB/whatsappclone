6b6c5ab2c1e673f67679322ba00f3d1be8881426
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2021-06-16 09:40:08 -0300

    Sidebar ChatList (1/2)

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -3,8 +3,23 @@ import "./ChatListItem.css";
 
 export default () => {
   return (
-    <div>
-      <div>...</div>
+    <div className="chatListItem">
+      <img
+        className="chatListItem--avatar"
+        src="https://www.w3schools.com/howto/img_avatar2.png"
+        alt=""
+      />
+      <div className="chatListItem--lines">
+        <div className="chatListItem--line">
+          <div className="chatListItem--name">Tiago da Silva</div>
+          <div className="chatListItem--date">19:00</div>
+        </div>
+        <div className="chatListItem--line">
+          <div className="chatListItem--lastMsg">
+            <p>Opa tudo bem</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
