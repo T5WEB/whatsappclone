6f1ed2c61c97f46ef44105b75cba24627d5cf685
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2021-06-17 18:19:47 -0300

    Configurando firebase no projeto

---
+++ src/firebaseconfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCSvxzVtr9NLyQpoekamScmtWDhoXpM0sQ",
+  authDomain: "whatsappclone-bbdc6.firebaseapp.com",
+  projectId: "whatsappclone-bbdc6",
+  storageBucket: "whatsappclone-bbdc6.appspot.com",
+  messagingSenderId: "198902705105",
+  appId: "1:198902705105:web:c780cc100dc2b3cec20ef0",
+};
